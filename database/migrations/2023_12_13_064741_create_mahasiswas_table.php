a38be1c56a319bedf59df671d11061d7c81d479a
ikwnnz <ikwn.nz@gmail.com>
2023-12-13 14:24:10 +0700

    Submission 4

--- database/migrations/2023_12_13_064741_create_mahasiswas_table.php
+++ database/migrations/2023_12_13_064741_create_mahasiswas_table.php
@@ -18,6 +18,7 @@ class CreateMahasiswasTable extends Migration
             $table->unique('nim');
             $table->string('nama');
             $table->string('jurusan');
+            
         });
     }
 
